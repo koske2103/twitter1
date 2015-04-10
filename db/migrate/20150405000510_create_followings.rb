4ae3b803fc057c3cb53f308941bbb2f5e3f5615b
Roque Perez <koske@hotmail.es>
2015-04-10 17:39:16 -0300

    cambio en la base de datos

--- db/migrate/20150405000510_create_followings.rb
+++ db/migrate/20150405000510_create_followings.rb
@@ -1,8 +1,8 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
     create_table :followings do |t|
-      t.belongs_to :user, index: true, foreign_key: true
-      t.belongs_to :following_user, index: true, foreign_key: true
+      t.belongs_to :user#, index: true, foreign_key: true
+      t.belongs_to :following_user#, index: true, foreign_key: true
 
       t.timestamps null: false
     end
